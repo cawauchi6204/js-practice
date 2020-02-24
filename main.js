7a00f434329e51d5eb098ac1df16a3ffac57a950
cawauchi6204 <cawauchi6204@gmail.com>
2020-02-24 16:09:40 +0900

    second commit

--- main.js
+++ main.js
@@ -1,24 +1,35 @@
+
 // fizzbuzz課題
 
-document.getElementById('button').addEventListener('click',function() {
-  const fizzNum = document.getElementById('fizzInput').value;
-  const buzzNum = document.getElementById('buzzInput').value; 
-  const fn = parseInt(fizzNum,10);
-  const bn = parseInt(buzzNum,10);
-  if(!isNaN(fn) && !isNaN(bn)) {
-    const fizz = document.createElement('li');
-    fizz.textContent = fn;
-    const buzz = document.createElement('li')
-    buzz.textContent = bn;
-    const ul1 = document.querySelector('ul');
-    const ul2 = document.querySelector('ul');
-    ul1.appendChild(fizz);
-    ul2.appendChild(buzz);
-    
-}else {
-  const nan = document.createElement('li');
-  nan.textContent ="整数値ではありません";
-  const ul3 = document.querySelector('ul');
-  ul3.appendChild(nan);
+const btn = document.getElementById('button');
+btn.addEventListener('click',function() {
+const fizzNum = document.getElementById('fizzInput').value;
+const buzzNum = document.getElementById('buzzInput').value; 
+const fn = parseInt(fizzNum,10);
+const bn = parseInt(buzzNum,10);
+const li = document.createElement('li')
+const ul = document.querySelector('ul');
+if(!isNaN(fn) && !isNaN(bn)) {
+for(let i = 1; i <= 100; i++) {
+if ( i % fn == 0  && i % bn == 0 ) {
+li.textContent +=('fizzbuzz:'+ i +'\n');
+ul.appendChild(li);
+}else if(i % fn === 0) {
+li.textContent +=('fizz:'+ i +'\n');
+ul.appendChild(li);
+}else if ( i % bn === 0 ) {
+li.textContent +=('buzz:'+ i + '\n');
+ul.appendChild(li);
+}
+}
+} else {
+if(ul) {
+ul.textContent = null;
+const nan = document.createElement('li');
+nan.textContent ="整数値ではありません";
+const ulNan = document.querySelector('ul');
+ulNan.appendChild(nan);
+}
 }
 })
+
