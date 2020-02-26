89f86dceba2acf65ea2a58b7145ab88a23bb943c
cawauchi6204 <cawauchi6204@gmail.com>
2020-02-26 13:44:04 +0900

    third commit

--- main.js
+++ main.js
@@ -3,33 +3,30 @@
 
 const btn = document.getElementById('button');
 btn.addEventListener('click',function() {
-const fizzNum = document.getElementById('fizzInput').value;
-const buzzNum = document.getElementById('buzzInput').value; 
-const fn = parseInt(fizzNum,10);
-const bn = parseInt(buzzNum,10);
-const li = document.createElement('li')
-const ul = document.querySelector('ul');
-if(!isNaN(fn) && !isNaN(bn)) {
-for(let i = 1; i <= 100; i++) {
-if ( i % fn == 0  && i % bn == 0 ) {
-li.textContent +=('fizzbuzz:'+ i +'\n');
-ul.appendChild(li);
-}else if(i % fn === 0) {
-li.textContent +=('fizz:'+ i +'\n');
-ul.appendChild(li);
-}else if ( i % bn === 0 ) {
-li.textContent +=('buzz:'+ i + '\n');
-ul.appendChild(li);
-}
-}
-} else {
-if(ul) {
-ul.textContent = null;
-const nan = document.createElement('li');
-nan.textContent ="整数値ではありません";
-const ulNan = document.querySelector('ul');
-ulNan.appendChild(nan);
-}
-}
-})
+  const fizzNum = document.getElementById('fizzInput').value;
+  const buzzNum = document.getElementById('buzzInput').value; 
+  const fn = parseInt(fizzNum,10);
+  const bn = parseInt(buzzNum,10);
+  const li = document.createElement('li');
+  const ul = document.querySelector('ul');
+  if(!isNaN(fn) && !isNaN(bn)) {
+    for(let i = 1; i <= 100; i++) {
+      if ( i % fn === 0  && i % bn === 0 ) {
+        li.innerHTML += ('fizzbuzz:'+ i + '<br>');
+        ul.appendChild(li);
+      }else if(i % fn === 0) {
+        li.innerHTML += ('fizz:'+ i + '<br>');
+        ul.appendChild(li);
+      }else if ( i % bn === 0 ) {
+        li.innerHTML += ('buzz:'+ i + '<br>' );
+        ul.appendChild(li);
+      }
+      }
+    } else {
+        ul.innerHTML ='';
+        const nan = document.createElement('li');
+        nan.innerHTML = "整数値ではありません";
+        ul.appendChild(nan);
+      }
+  })
 
